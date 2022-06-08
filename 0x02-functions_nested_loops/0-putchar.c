11c0bf6753d77997a8f22cd42fc940a75d2aa761
Ian Gitau <ianmchr9@gmail.com>
2022-06-08 13:41:33 -0700

    Header&_putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * main - prints _putchar and ends in a new line
+ *
+ * Return: return 0 (Success)
+ */
+int main(void)
+{
+	int i;
+
+	char p[10] = "_putchar"
+
+	for (i = 0; i < 8; i++)
+        {
+	        _putchar(p[i]);
+	}
+        _putchar(10);
+	return (0);
+}	
