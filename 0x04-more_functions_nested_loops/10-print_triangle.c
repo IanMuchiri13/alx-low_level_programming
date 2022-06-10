0e688906a450aba7c731531a932851e4110efc25
Ian Gitau <ianmchr9@gmail.com>
2022-06-10 16:38:11 -0700

    triangles

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -12,7 +12,7 @@ void print_triangle(int size)
 		_putchar('\n');
 	for (row = 0; row < size; row++)
 	{
-		for (column = size; column > 1; column--)
+		for (column = size - row; column > 1; column--)
 			_putchar(' ');
 		for (k = row + column; k >= 1; k--)
 			_putchar('#');
