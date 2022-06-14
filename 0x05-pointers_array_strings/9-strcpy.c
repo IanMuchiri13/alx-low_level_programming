c2650c177104906d4c194eca157422ff72543f82
Ian Gitau <ianmchr9@gmail.com>
2022-06-13 18:31:26 -0700

    String pointed copied

---
+++ 0x05-pointers_array_strings/9-strcpy.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _strcpy - copies the string pointed
+ * @dest: value to be evaluated
+ * @src: value to be evaluated
+ *
+ * Return: Success
+ */
+char *_strcpy(char *dest, char *src)
+{
+	int l = 0;
+	int x = 0;
+
+	while (*(src + l) != '\0')
+	{
+		l++;
+	}
+	for ( ; x < l; x++)
+	{
+		dest[x] = src[x];
+	}
+	dest[l] = '\0';
+	return (dest);
+}
